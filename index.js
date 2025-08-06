979b9c421878b5fdc71413721d3ea1bb9eb80c23
dipu <deepshrestha365@gmail.com>
2025-08-06 10:42:57 +0545

    fixing crud issue on local

--- index.js
+++ index.js
@@ -116,15 +116,7 @@ app.get("/products", (req, res) => {
 });
 
 // Get single product
-// app.get("/products/:id", (req, res) => {
-//   const product = products.find((p) => p.id === req.params.id);
-//   if (!product) return res.status(404).json({ message: "Product not found" });
-//   res.json(product);
-// });
-
 app.get("/products/:id", (req, res) => {
-  const products = getProducts(); // Just get the products without sending response
-  console.log("Products loaded:", products);
   const product = products.find((p) => p.id === req.params.id);
   if (!product) return res.status(404).json({ message: "Product not found" });
   res.json(product);
