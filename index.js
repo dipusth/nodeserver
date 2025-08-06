7edd17b4fc26b602ef10b008cb67ef2697971b3e
dipu <deepshrestha365@gmail.com>
2025-08-06 11:19:38 +0545

    saving products

--- index.js
+++ index.js
@@ -230,6 +230,7 @@ app.put("/products/:id", upload.single("image"), async (req, res) => {
     };
 
     products[productIndex] = updatedProduct;
+    saveProducts(products);
     await fs.promises.writeFile(
       getProductsPath(),
       JSON.stringify(products, null, 2)
